b22a3687314fb472eb3e15f0ae74dcea3b20ad40
Rakshak1344 <dev.rakshith14@gmail.com>
2025-03-04 10:35:21 +0530

    chore: Resume link update.

--- lib/arch/config/links.dart
+++ lib/arch/config/links.dart
@@ -12,7 +12,7 @@ class My {
       'https://www.linkedin.com/in/rakshith-rakshak-4bb720173/';
   final String twitter = 'https://twitter.com/Rakshithdev14';
   final String resume =
-      'https://drive.google.com/file/d/1bKXzDAnnU8D0ipjFtKwdZoz45R94fB3q/view?usp=sharing';
+      'https://drive.google.com/file/d/1zCcdpKyIwZSkNb-_pvnd9WJ4GeGfoGLx/view?usp=sharing';
 
   /// Github
   final String github = 'https://github.com/Rakshak1344';
