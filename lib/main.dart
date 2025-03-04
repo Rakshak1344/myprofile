4bb94c480ccf388fd96e42f75467180cb31b93dc
Rakshak1344 <dev.rakshith14@gmail.com>
2025-03-04 18:11:08 +0530

    chore: removed setPathUrl.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:profile/my_app.dart';
 import 'package:profile/my_app_config.dart';
 import 'package:profile/splash_app.dart';
 
 void main() {
-  if(kIsWeb){
-    usePathUrlStrategy();
-  }
-
   runApp(const SplashApp());
 
   MyAppConfig().init(child: const MyApp()).then((widget) => runApp(widget));
