dca940af069a47c930027ec1a88867e8e35af150
Rakshak1344 <dev.rakshith14@gmail.com>
2025-06-29 17:59:05 +0530

    chore: removed icon convertor

--- lib/features/socials/data/models/social_info_data.dart
+++ lib/features/socials/data/models/social_info_data.dart
@@ -2,22 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:profile/arch/config/links.dart';
-import 'package:profile/features/common/data/convertors/icon_data_convertor.dart';
 
 part 'social_info_data.freezed.dart';
-
-part 'social_info_data.g.dart';
 
 @freezed
 class SocialInfoData with _$SocialInfoData {
   const factory SocialInfoData({
     required String name,
-    @IconDataConverter() required IconData iconData,
+    required IconData iconData,
     required String url,
   }) = _SocialInfoData;
-
-  factory SocialInfoData.fromJson(Map<String, dynamic> json) =>
-      _$SocialInfoDataFromJson(json);
 
   static List<SocialInfoData> get socialInfoData => <SocialInfoData>[
         SocialInfoData(
